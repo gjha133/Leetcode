ff7d03396ee8e7e723e591047091ac48029851b1
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-03-24 04:38:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.88%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        int count = 0;
+        while(target > startValue){
+            if(target&1) target++;
+            else target /= 2;
+            count++;
+        }
+        return count + (startValue - target);
+    }
+};
