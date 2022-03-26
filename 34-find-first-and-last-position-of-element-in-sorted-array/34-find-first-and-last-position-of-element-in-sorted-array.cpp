345201b300780cf869e9c39690cfd0a234de839e
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-03-27 02:46:40 +0530

    Time: 11 ms (57.48%), Space: 13.5 MB (98.37%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,27 +4,27 @@ public:
         vector<int> pos;
         int start = 0;
         int end = nums.size() - 1;
-        int ans = -1;
+        int first = -1;
         while(start <= end)
         {
             int mid = start + (end-start)/2;
-            if(nums[mid] == target)     {ans = mid;     end = mid-1;}
+            if(nums[mid] == target)     {first = mid;     end = mid-1;}
             else if(nums[mid] > target) end = mid-1;
             else start = mid + 1;
         }
-        cout << ans << endl;
-        pos.push_back(ans);
-        ans = -1;
+        pos.push_back(first);
+        int last = -1;
         start = 0;
         end = nums.size() - 1;
         while(start <= end)
         {
             int mid = start + (end-start)/2;
-            if(nums[mid] == target)     {ans = mid;     start = mid + 1;}
+            if(nums[mid] == target)     {last = mid;     start = mid + 1;}
             else if(nums[mid] > target) end = mid-1;
             else start = mid + 1;
         }
-        pos.push_back(ans);  
+        pos.push_back(last); 
+        cout << "Count : " << (last - first + 1);
         return pos;
     }
 };
