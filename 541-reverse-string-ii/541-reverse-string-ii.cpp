49786273dea3c1c477ee20a0796179b29e52e9db
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-03-28 03:11:43 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (71.96%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+     string reverseStr(string s, int k) {
+        int i = 0, n = s.length();
+        while(i<n) {
+            int j = min(i+k-1, n-1);
+            int nexti = j+k+1;
+            while(i<j) swap(s[i++], s[j--]);
+            i = nexti;
+        }
+        return s;
+    }
+    
+};
+
+
