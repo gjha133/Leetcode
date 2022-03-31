394f39709a836342fa748f996c8a6e2fff649613
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-03-31 19:27:49 +0530

    Time: 113 ms (30.12%), Space: 32.1 MB (93.38%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        vector<vector<int>> output;
-        int diff = INT_MAX;
-        int newdiff;
-        for(int i=1; i<arr.size(); i++)
-        {
-            newdiff = abs(arr[i] - arr[i-1]);            
-            if(diff > newdiff) 
-            {
-                while(!output.empty()) output.pop_back();
-                vector<int> ans {arr[i-1], arr[i]};
-                output.push_back(ans);
+    vector<vector<int>> minimumAbsDifference(vector<int>& A) {
+        sort(begin(A), end(A));
+        vector<vector<int>> ans;
+        int minDiff = INT_MAX, curDiff;
+        for(int i = 1; i < size(A); i++) {
+            auto [a, b] = minmax(A[i], A[i-1]);                 // a and b will hold min and max amongst A[i] and A[j]
+            curDiff = b - a;
+            if(curDiff == minDiff) ans.push_back({a, b});       // push if equal to MAB found till now
+            else if(curDiff < minDiff) {                        // if current diff < MAB, update it and only keep current pair in ans
+                minDiff = curDiff;
+                ans = {{a, b}};
             }
-            if(diff == newdiff)
-            {
-                vector<int> ans {arr[i-1], arr[i]};
-                output.push_back(ans);
-            }
-            diff = min(diff, newdiff);
         }
-        
-        return output;
-        
-        
+        return ans;
     }
 };
