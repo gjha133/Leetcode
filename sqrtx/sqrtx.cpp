942e616498d0dfb377127f546afbdc8dbfa23900
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-03-31 23:26:15 +0530

    Time: 6 ms, Memory: 5.9 MB - LeetHub

---
+++ sqrtx/sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<=1) return x;
+        int start = 1;
+        int end = x/2;
+        long square;
+        int ans = -1;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            square = long(mid)*long(mid);
+           // square = mid * mid;
+            if(square == x) return mid;
+            if(square < x) {start = mid + 1; ans = mid;}
+            else end = mid - 1;
+        }
+        
+        return ans;
+    }
+};
