f39d11cd1d3d6df2adb2e1157f460c2c0034b6da
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-04-03 03:42:23 +0530

    Time: 76 ms (36.95%), Space: 24 MB (39.76%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     bool ispalindrome(string s, int i, int j){
-        while(i < j) if(s.at(i++)!= s.at(j--)) return false;        
+        while(i < j){
+            if(s.at(i) == s.at(j)){
+                i++;
+                j--;
+            }else return false;
+        }
         return true;
     }
     bool validPalindrome(string s) {
         int i  = 0;
         int j = s.size()-1;
-        while(i < j)
-        {
-            if(s.at(i) != s.at(j)) return ispalindrome(s, i+1, j) || ispalindrome(s, i, j-1);
-            else {i++; j--;}
+        while(i < j){
+            if(s.at(i) == s.at(j)){
+                i++;
+                j--;
+            }else{
+                return ispalindrome(s, i+1, j) || ispalindrome(s, i, j-1);
+            }
         }
         return true;
     }
