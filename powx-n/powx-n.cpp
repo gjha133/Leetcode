96aab0670c6866dd4514a4bc98895e2021535286
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-04-10 01:19:05 +0530

    Time: 0 ms, Memory: 5.8 MB - LeetHub

---
+++ powx-n/powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        long long N = n;
+        if(N < 0) N = -1 * N;
+        
+        while(N)
+        {
+            if(N % 2 == 0) {x *= x;     N = N/2;}
+            else           {ans *= x;   N = N - 1;}
+        }
+        
+        if(n < 0) ans = double(1.0)/double(ans);
+        return ans;
+    }
+};
