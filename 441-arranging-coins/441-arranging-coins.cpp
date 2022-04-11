5e0424be6252d4a732b2df116376575cadc52392
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-04-12 01:02:50 +0530

    Time: 4 ms (70.58%), Space: 5.9 MB (27.35%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        
+        int start = 1, end = n;
+        while(start + 1 < end)
+        {
+            long mid = start + (end-start)/2;
+            long x = mid*(mid + 1)/2;
+            if(x > n) end = mid;
+            else start = mid;
+        }
+        
+        return start;
+    }
+};
