9dac467bd4744aa150b110c53070f2bf17e498b9
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-04-11 17:53:45 +0530

    Time: 18 ms (22.74%), Space: 10.2 MB (44.14%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int cz = coordinates.size();
+        if (cz == 1) return false;  // One point only, not a straight line
+        if (cz == 2) return true;  // Two points only, always a straight line
         
-        int dY = coordinates[1][1] - coordinates[0][1];
-	    int dX = coordinates[1][0] - coordinates[0][0];     
-	    for( int i=2; i < coordinates.size(); i++ ) {
-		    auto p = coordinates[i];
-		    if( dX*(p[1] - coordinates[0][1]) != dY*(p[0] - coordinates[0][0]) )
+        int x0 = coordinates[0][0], y0 = coordinates[0][1];
+        int dx = coordinates[1][0] - x0, dy = coordinates[1][1] - y0;
+        
+        for (int i = 1; i < cz; i++) {   // Checking two point formula for each point with the first point
+            int x = coordinates[i][0], y = coordinates[i][1];
+            if (dx * (y - y0) != dy * (x - x0))  // Two point formula of line, if (x2-x1)*(y1-y0) = (x1-x0)(y2-y1), then a straight line, otherwise not
 			    return false;
-	    }
-	    return true;
+        }
+        return true;
     }
 };
