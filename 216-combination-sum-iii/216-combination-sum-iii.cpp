3c44ceed375a9315525ce1657d3cc1b554b0f740
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-04-16 01:31:52 +0530

    Time: 4 ms (32.65%), Space: 6.3 MB (94.00%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    void findcombination(vector<int>& candidates, int target, int sum, vector<int> &ds, int index, int k, vector<vector<int>> &ans){
+        if(sum > target) return;
+        
+        if(sum == target)
+        {
+            if(k==0) ans.push_back(ds);
+            return;
+        }
+        
+        for(int i = index; i < candidates.size(); i++)
+        {
+            ds.push_back(candidates[i]); 
+            findcombination(candidates, target, sum + candidates[i], ds, i+1, k-1, ans);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> ds;
+        vector<vector<int>> ans;
+        vector<int> candidates;
+        for(int i=1;i<=9;i++) candidates.push_back(i);     
+        
+        findcombination(candidates,n,0,ds,0,k, ans);
+        
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
