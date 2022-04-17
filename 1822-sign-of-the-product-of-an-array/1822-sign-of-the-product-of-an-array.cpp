f2f2a9371b880bd18bcf2eb79e62e582a52d94be
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-04-17 16:59:13 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (86.16%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -5,8 +5,8 @@ public:
         
         for(int num : nums)
         {
-            if(num == 0) return 0;
             if(num < 0) ++count;
+            else if(!num) return 0;    // num == 0
         }
         
         return count&1 == 1 ? -1 : 1;
