bcf78e8caedd7875c157ee59bb04ff2cd94ad5a4
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-04-18 17:16:01 +0530

    Time: 20 ms (91.88%), Space: 19.4 MB (14.09%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
-        int zeroCount = 0;
-        for(int num : nums)
-        {
-            if(num == 0) ++zeroCount;
-        }
-        
+                
         int start = 0, i = 0;
-        for(int i=0; i < nums.size(); i++)
+        for(; i < nums.size(); i++)
         {
             if(nums[i] != 0)
             {
