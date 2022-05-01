091bec4e61efcc92e8475e16065e0f817efa50d0
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-05-01 10:47:56 +0530

    Time: 4 ms (28.12%), Space: 6.1 MB (88.17%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -4,41 +4,19 @@ public:
         int k=0,p=0;
         for(int i=0;i<s.size();i++)
         {
-            if(s[i]=='#')
-            {
-                k--;
-                 k=max(0,k);
-            }
-            
-           else
-           {
-               s[k]=s[i];
-               k++;
-           }
+            if(s[i]=='#') k=max(0,--k);            
+            else s[k++]=s[i];
         }
         for(int i=0;i<t.size();i++)
         {
-            if(t[i]=='#')
-            {
-                p--;
-                 p=max(0,p);
-            }
-            
-           else
-           {
-               t[p]=t[i];
-               p++;
-           }
+            if(t[i]=='#') p=max(0,--p);
+            else t[p++]=t[i];
         }
-        if(k!=p)
-            return false;
+        
+        if(k!=p) return false;
         else
         {
-            for(int i=0;i<k;i++)
-            {
-                if(s[i]!=t[i])
-                    return false;
-            }
+            for(int i=0;i<k;i++) if(s[i]!=t[i]) return false;
             return true;
         }
         
