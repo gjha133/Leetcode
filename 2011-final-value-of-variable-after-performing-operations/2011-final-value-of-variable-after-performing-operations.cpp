26b5f4aafc0a209c26eac075976bb16494ab57e4
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-06-05 03:20:46 +0530

    Time: 12 ms (53.54%), Space: 14.1 MB (10.30%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
+    
+    
     int finalValueAfterOperations(vector<string>& operations) {
-        int ans = 0;
-        for(string s : operations)
-        {
-            if(s == "--X" or s == "X--") ans--;
-            else ans++;
-        }
-        
-        return ans;
-    }
+		int res = 0;
+		for(int i = 0; i < operations.size(); i++){
+			(operations[i][1] == '+') ? res++ : res--;
+		}
+		return res;
+	}
 };
