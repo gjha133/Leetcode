7f461be28a727e3104eafd9b44d87fa8c84fb849
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-07-01 19:54:10 +0530

    Time: 18 ms (78.53%), Space: 11 MB (15.26%) - LeetHub

--- 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.cpp
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.cpp
@@ -21,11 +21,17 @@ public:
 class Solution {
 public:
     int maxDepth(Node* root) {
-        if(!root) return 0;
-        int Depth = 0;
-        
-        for(auto child : root->children) Depth = max(Depth, maxDepth(child));
-        
-        return 1 + Depth;
+        if (root == nullptr) return 0;
+        queue<Node*> q; q.push(root);
+        int depth = 0;
+        while (!q.empty()) {
+            depth += 1;
+            int breadth = q.size();
+            for (int _ = 0; _ < breadth; ++_) {
+                auto node = q.front(); q.pop();
+                for (auto child : node->children) if (child) q.push(child);
+            }
+        }
+        return depth;
     }
 };
