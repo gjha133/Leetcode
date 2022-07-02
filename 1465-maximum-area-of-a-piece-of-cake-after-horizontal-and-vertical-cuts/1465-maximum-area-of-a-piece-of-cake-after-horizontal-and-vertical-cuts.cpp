40a4a1b06ffc095efb59392bd1eea619b8abb3ea
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-07-03 03:37:48 +0530

    Time: 76 ms (95.12%), Space: 32.4 MB (39.84%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -11,20 +11,25 @@ public:
         
         for(int i=0;i<n;++i){
             if(i==0)
-                maxVerticalDis = max(maxVerticalDis,horizontalCuts[i]);
+                //Assigning max vertical distance to distance from 0 to first horizontal cut
+                maxVerticalDis = max(maxVerticalDis,horizontalCuts[i]); 
+            
             else
                 maxVerticalDis = max(maxVerticalDis,horizontalCuts[i] - horizontalCuts[i-1]);
         }
         
+        // Checking if the distance between the last horizontal cut and h is greater than the cut in between
         maxVerticalDis = max(maxVerticalDis,h-horizontalCuts[n-1]);
         
         for(int i=0;i<m;++i){
             if(i==0)
+                //Assigning max horizontal distance to distance from 0 to first vertical cut
                 maxHorizontalDis = max(maxHorizontalDis,verticalCuts[i]);
             else
                 maxHorizontalDis = max(maxHorizontalDis,verticalCuts[i] - verticalCuts[i-1]);
         }
         
+        // Checking if the distance between the last vertical cut and w is greater than the cut in between
         maxHorizontalDis = max(maxHorizontalDis,w-verticalCuts[m-1]);
         
         return (long) maxHorizontalDis*maxVerticalDis % 1000000007;
