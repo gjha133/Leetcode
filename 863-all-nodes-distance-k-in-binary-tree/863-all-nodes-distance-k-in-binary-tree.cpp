dddf58e7a554c35f72e808ec7b69cd6abc9e0204
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-07-06 15:15:03 +0530

    Time: 7 ms (76.46%), Space: 13 MB (46.31%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -14,38 +14,55 @@ public:
         unordered_map<N*, N*> parent;
         markParent(root, parent);
         
+        // Taking a map for all the visited nodes
         unordered_map<N*, bool> visited;
         queue<N*> q;
+        // Push the target node in the queue
         q.push(target);
+        // Mark the target node visited
         visited[target] = true;
+        // Taking distance = 0
         int dist = 0;
         
         while(!q.empty())
         {
             int n = q.size();
+            // If distance traversed is equal to k, we break out of loop 
             if(dist++ == k) break;
             
             for(int i=0; i<n; i++)
             {
-                N* front = q.front();     q.pop();
-                if(front->left and !visited[front->left])
+                N* node = q.front();     q.pop();
+                
+                // If left child exists and we have not visited it
+                if(node->left and !visited[node->left])
                 {
-                    q.push(front->left);
-                    visited[front->left] = true;
+                    // Push the left child in the queue and mark it visited
+                    q.push(node->left);
+                    visited[node->left] = true;
                 }
-                if(front->right and !visited[front->right])
+                
+                // If right child exists and we have not visited it
+                if(node->right and !visited[node->right])
                 {
-                    q.push(front->right);
-                    visited[front->right] = true;
+                    // Push the right child in the queue and mark it visited
+                    q.push(node->right);
+                    visited[node->right] = true;
                 }
-                if(parent[front] and !visited[parent[front]])
+                
+                // If Parent node exists and we have not visited it. 
+                // For example if target node is our root...its parent doesnt exists, so it is better to check if it exists or not
+                if(parent[node] and !visited[parent[node]])
                 {
-                    q.push(parent[front]);
-                    visited[parent[front]] = true;
+                    // Push parent node in the queue and mark it visited
+                    q.push(parent[node]);
+                    visited[parent[node]] = true;
                 }
             }
         }
         
+        // After breaking out of loop if distance == k, the remaining nodes in queue is our answer
+        // Add the remaining nodes to the vector
         vector<int> ans;
         while(!q.empty())
         {
@@ -58,20 +75,22 @@ public:
     
     void markParent(N* root, unordered_map<N*, N*> &parent)
     {
+        // Mark parents of all the nodes in the unordered map
+        
         queue<N*> q;
         q.push(root);           
         while(!q.empty())
         {
-            N *front = q.front();   q.pop();
-            if(front->left) 
+            N *node = q.front();   q.pop();
+            if(node->left) 
             {
-                parent[front->left] = front;
-                q.push(front->left);
+                parent[node->left] = node;
+                q.push(node->left);
             }
-            if(front->right)
+            if(node->right)
             {
-                parent[front->right] = front;
-                q.push(front->right);
+                parent[node->right] = node;
+                q.push(node->right);
             }
         }
     }
