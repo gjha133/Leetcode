8820568fa3f108b4e2a940c448b08a39826ad459
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-07-15 03:16:07 +0530

    Time: 15 ms (99.89%), Space: 13.6 MB (59.95%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-    int n = graph.size();
-    vector<int> color(n); // 0: uncolored; 1: color A; -1: color B
-        
-    queue<int> q; // queue, resusable for BFS    
-	
-    for (int i = 0; i < n; i++) {
-      if (color[i]) continue; // skip already colored nodes
-      
-      // BFS with seed node i to color neighbors with opposite color
-      color[i] = 1; // color seed i to be A (doesn't matter A or B) 
-      for (q.push(i); !q.empty(); q.pop()) {
-        int cur = q.front();
-        for (int neighbor : graph[cur]) 
-		{
-          if (!color[neighbor]) // if uncolored, color with opposite color
-          { color[neighbor] = -color[cur]; q.push(neighbor); } 
-		  
-          else if (color[neighbor] == color[cur]) 
-            return false; // if already colored with same color, can't be bipartite!
-        }        
-      }
-    }
+    bool isBipartite(vector<vector<int>>& graph) 
+    {
+        int V = graph.size();
+
+        // 0: uncolored; 1: color A; -1: color B
+        vector<int> color(V); 
+
+        queue<int> q; //For BFS    
+
+        for (int i = 0; i < V; i++) 
+        {
+            if (color[i]) continue; // skip already colored nodes
+
+            // BFS with seed node i to color neighbors with opposite color
+            color[i] = 1; // color seed i to be A (doesn't matter A or B) 
+            for (q.push(i); !q.empty(); q.pop()) 
+            {
+                int node = q.front();
+                for (int neighbor : graph[node]) 
+                {
+                    if (!color[neighbor]) // if uncolored, color with opposite color
+                    { 
+                        color[neighbor] = -color[node]; 
+                        q.push(neighbor); 
+                    } 		  
+                    else
+                    {
+                        if (color[neighbor] == color[node])    // if already colored with same color, can't be bipartite!
+                            return false;       
+                    }
+
+                }        
+            }
+        }
     
-    return true;
+        return true;
   }
 };
