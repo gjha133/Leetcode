50a90adc07ba5bbc0af94facc5e7d82d7e89347a
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-07-27 04:57:52 +0530

    Time: 69 ms (78.52%), Space: 16.6 MB (75.37%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -2,29 +2,16 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         int freq[26] = {0};
-        /*
         for(int i=0; i<s.size(); i++)
         {
             freq[s[i] - 'a']++;
-        }
-        */
-        for(int i=0; i<s.size(); i++)
-        {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
         }
         
         int ans = 0;
-        /*
         for(int i=0; i<s.size(); i++)
         {
             if(freq[t[i] - 'a'] > 0) freq[t[i] - 'a']--;
             else ans++;
-        }
-        */
-        for(int i=0; i<26; i++)
-        {
-            if(freq[i] > 0) ans += freq[i];
         }
         
         return ans;
