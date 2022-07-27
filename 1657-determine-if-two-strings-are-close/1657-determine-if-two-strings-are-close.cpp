11fb3e3c29794ccf7e9fa44fbf1ba94febb2dec3
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-07-27 05:58:10 +0530

    Time: 102 ms (73.54%), Space: 20.4 MB (84.05%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,37 +1,49 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        
+               
         /*
-        if(word1.size() != word2.size()) return false;
-        int freq1[26] = {0}, freq2[26] = {0};
-        set<char> s1, s2;
-        for(int i=0; i<word1.size(); i++)
-        {
-            freq1[word1[i] - 'a']++;
-            freq2[word2[i] - 'a']++;
-            if( s1.find(word1[i]) == s1.end() ) s1.insert(word1[i]);
-            if( s2.find(word2[i]) == s2.end() ) s2.insert(word2[i]);
-        }
-        
-        sort(freq1, freq1 + 26);
-        sort(freq2, freq2 + 26);
-        
-        return s1 == s2 and freq1 == freq2;
-        */
-        
         if( word1.length() != word2.length() ) return false;
+        
         set<char> s1, s2;
         vector<int> w1(26,0), w2(26,0);
+        
         for(int i=0; i<word1.length(); i++){
             w1[word1[i]-'a']++;
-            w2[word2[i]-'a']++;
             s1.insert(word1[i]);
+            
+            w2[word2[i]-'a']++;            
             s2.insert(word2[i]);
         } 
-        std::sort(w1.begin(), w1.end());
-        std::sort(w2.begin(), w2.end());
-        return s1 == s2 && w1 == w2;
         
+        sort(w1.begin(), w1.end());
+        sort(w2.begin(), w2.end());
+        
+        return s1 == s2 and w1 == w2;     
+        */
+        
+        vector<int> freq1(26,0), freq2(26,0), vis1(26,0), vis2(26,0);
+        
+        for(char c : word1)
+        {
+            // For frequency 
+            freq1[c-'a']++;
+            // Marking visited char as 1
+            vis1[c-'a'] = 1;
+        }
+            
+    
+        for(char c : word2)
+        {
+            // For frequency 
+            freq2[c-'a']++;
+            // Marking visited char as 1
+            vis2[c-'a'] = 1;
+        }           
+            
+        sort(begin(freq1),end(freq1));
+        sort(begin(freq2),end(freq2));
+        
+        return freq1 == freq2 and vis1 == vis2;
     }
 };
