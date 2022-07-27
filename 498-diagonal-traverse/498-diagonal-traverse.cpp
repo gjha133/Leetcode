b556cbbfa53620171de3a5a3058bdc270f5d5f70
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-07-27 21:01:24 +0530

    Time: 49 ms (58.59%), Space: 17.6 MB (99.55%) - LeetHub

---
+++ 498-diagonal-traverse/498-diagonal-traverse.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+        
+        vector<int> ans (rows*cols);
+        
+        int row = 0, col = 0;
+        for(int i=0; i<rows*cols; i++)
+        {
+            ans[i] = mat[row][col];
+            // Checking sum of row + col
+            if(!((row + col)&1))  // If the sum is even. We will go in upward direction. row-- col++
+            {
+                // If it is the last column. We cant go up so we will move to next row
+                if(col == cols-1) row++; 
+                // If it is the first row. We will move to next column
+                else if(row == 0) col++;
+                // Simply move diagonally up
+                else row--, col++;
+            }
+            else //If the sum is odd. We will go in downward direction. row++ col--
+            {
+                // If it is the last row. We cant go down so we will move to next col (8->9)
+                if(row == rows-1) col++;
+                // If it is the first col. We will move to next row (4->7)
+                else if(col == 0) row++;
+                // Simply move digonally down
+                else row++, col--;
+            }
+        }
+        return ans;
+    }
+};
