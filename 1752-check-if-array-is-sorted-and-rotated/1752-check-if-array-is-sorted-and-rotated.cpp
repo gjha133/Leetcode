1a58b83b3a79978e6adbcc1da2e04985aec162f6
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-07-28 17:47:01 +0530

    Time: 4 ms (46.95%), Space: 8.3 MB (75.82%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,18 +4,18 @@ public:
         int count = 0;
         int n = nums.size();
         
-        for(int i=0; i<n; i++)
+        for(int i=1; i<n; i++)
         {
-            if(nums[i] > nums[ (i+1) % n ]) count++;
-            if(count == 2) return false;
-            
-            /*
-            In case of [2, 1, 3, 4] 
-            If array is sorted and rotated, num[0] > num[4]
-            */
+            if(nums[i-1] > nums[i]) count++;            
         }
+        /*
+            In case of [2, 1, 3, 4] 
+            If array is sorted and rotated, nums[0] > nums[n-1]
+        */
         
-        return true;
+        if(nums[0] < nums[n-1]) count++;
+        
+        return count <= 1;
         
     }
 };
