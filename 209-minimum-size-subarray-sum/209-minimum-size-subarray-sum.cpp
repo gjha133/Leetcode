df381483bfa47e3170c09d105d874836f29bae88
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-04 15:49:42 +0530

    Time: 36 ms (67.89%), Space: 28.3 MB (36.77%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -6,17 +6,13 @@ public:
         while( j < n )
         {
             sum += nums[j];
-            if(sum < target) j++;
-            else
+            while(sum >= target)
             {
-                while(sum >= target)
-                {
-                    mn = min(mn, j - i + 1);
-                    sum -= nums[i];
-                    i++;
-                }
-                j++;
+                mn = min(mn, j - i + 1);
+                sum -= nums[i];
+                i++;
             }
+            j++;
         }
         
         return mn == INT_MAX ? 0 : mn;
