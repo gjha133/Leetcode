f99aab78824f7aed1eb3ebab54812eea9e8be611
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-04 17:56:33 +0530

    Time: 46 ms (26.67%), Space: 9 MB (33.15%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -8,22 +8,15 @@ public:
         while( j < n )
         {
             mp[s[j]]++;
+            while(mp.size() < j - i + 1)
+            {
+                mp[s[i]]--;
+                if(mp[s[i]] == 0) mp.erase(s[i]);
+                i++;
+            }
             if(mp.size() == j - i + 1)
             {
                 mx = max(mx, j - i + 1);
-            }
-            else if(mp.size() < j - i + 1)
-            {
-                while(mp.size() < j - i + 1)
-                {
-                    mp[s[i]]--;
-                    if(mp[s[i]] == 0) mp.erase(s[i]);
-                    i++;
-                }
-                if(mp.size() == j - i + 1)
-                {
-                    mx = max(mx, j - i + 1);
-                }
             }
             j++;
         }
