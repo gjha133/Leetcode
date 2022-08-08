97a9c0f514188a5f86611fb5b2a560f1c0f54255
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-08 23:44:53 +0530

    Time: 2 ms (60.46%), Space: 6 MB (20.10%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    bool isUgly(int num) {        
-        vector<int> factors = {2, 3, 5};
-        if (num < 1) return false;
-        for (int prime: factors) 
-            while (num % prime == 0) 
-                num /= prime;
+    bool isUgly(int n) {        
+        if(n == 0) return false;
         
-        return num == 1;
+        while(n != 1)
+        {
+            if(n % 2 == 0) n /= 2;
+            else if(n % 3 == 0) n /= 3;
+            else if(n % 5 == 0) n/= 5;
+            else return false;
+        }
+        
+        return n == 1;
+        
     }
 };
