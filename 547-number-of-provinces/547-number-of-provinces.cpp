258ef8e41be8a726c960956192ac45812694578d
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-09 00:46:02 +0530

    Time: 41 ms (45.08%), Space: 13.7 MB (63.72%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -2,35 +2,25 @@ class Solution {
 public:
     int findCircleNum(vector<vector<int>>& mat) {
         int V = mat.size();
-        vector<bool> vis(V, false);
-        vector<vector<int>> adj(V);
-        
-        // convert to adj list and find connected components
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                if (mat[i][j] == 1 and i != j)
-                {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
+        if(!V) return 0;
         int cnt = 0;
+        vector<bool> vis(V, 0);
         for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
+            if (!vis[i]) 
+            {
                 cnt++;
-                DFS(i, vis, adj);
+                DFS(i, vis, mat);
             }
         }
         return cnt;
     }
     
-    void DFS(int src, vector<bool>& vis, vector<vector<int>>& adj) {
+    void DFS(int src, vector<bool>& vis, vector<vector<int>>& mat) {
         vis[src] = true;
-        for (int u : adj[src]) {
-            if (!vis[u]) {
-                DFS(u, vis, adj);
-            }
+        for (int j=0; j<mat.size(); j++) 
+        {
+            if (!vis[j] and mat[src][j]) 
+                DFS(j, vis, mat);
         }
     }
 };
