36301acb5042535410c76893165a086b07f0458c
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-10 13:42:24 +0530

    Time: 277 ms (87.55%), Space: 110 MB (39.69%) - LeetHub

---
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    #define ll long long
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+        ll sum = 0;
+        deque<pair<ll,int>> dq;
+        int ans = INT_MAX;
+        for(int i=0; i<n; i++)
+        {
+            sum += nums[i];
+            if(sum >= k) ans = min(ans,i+1);
+            
+            while(!dq.empty() && sum-dq.front().first>=k)
+            {
+                ans = min(ans,i-dq.front().second);
+                dq.pop_front();
+            }
+            while(!dq.empty() && dq.back().first >= sum)
+                dq.pop_back();
+            
+            dq.push_back({sum,i});
+        }
+        
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
