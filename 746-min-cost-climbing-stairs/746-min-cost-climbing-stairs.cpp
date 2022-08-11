2be8395b98996863b8bc37049109217125fe6702
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-11 22:16:35 +0530

    Time: 16 ms (22.33%), Space: 14 MB (9.53%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size() + 1, -1);
+        return min(f(0, cost, dp), f(1, cost, dp)); 
+    }
+    
+    int f(int ind, vector<int> &cost, vector<int> &dp)
+    {
+        // If we go out of bound, we have reached the top of the floor
+        if(ind >= cost.size()) return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int oneStep = f(ind + 1, cost, dp) + cost[ind];
+        int twoStep = f(ind + 2, cost, dp) + cost[ind];
+        
+        return dp[ind] = min(oneStep, twoStep);
+    }
+};
