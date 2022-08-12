3cccf2fa57dc098bc8060d04cc37756128b3f56f
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-13 02:42:55 +0530

    Time: 3 ms (82.69%), Space: 7.6 MB (91.69%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -8,7 +8,7 @@ public:
     
     void permutation(vector<int>& nums, vector<vector<int>>& ans, int index)
     {
-        if(index == nums.size())
+        if(index == nums.size() - 1)
         {
             ans.push_back(nums);
             return;
@@ -22,3 +22,45 @@ public:
         }
     }
 };
+
+
+
+/*
+
+Solution:
+
+1st Method........Uses extra space for freq array
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        int freq[nums.size()];
+        for(int i=0; i<nums.size(); i++) freq[i] = 0;
+        permutation(nums, ans, ds, freq);
+        return ans;
+    }
+    
+    void permutation(vector<int>& nums, vector<vector<int>>& ans, vector<int>& ds, int freq[])
+    {
+        if(ds.size() == nums.size())
+        {
+            ans.push_back(ds);
+            return;
+        }
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(freq[i] == 0)
+            {
+                ds.push_back(nums[i]);
+                freq[i] = 1;
+                permutation(nums, ans, ds, freq);
+                freq[i] = 0;
+                ds.pop_back();
+            }
+        }
+    }
+};
+
+*/
