9a5ed1897aaaffb714171a7ff4c4888b45d46bc1
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-12 23:38:16 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (32.18%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    vector<string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans;
-    vector<string> letterCombinations(string digits) {
-	if(digits == "") return ans;        
-	string combination = "";
-	helper(digits, 0, combination);
-	return ans; 
-}
-//                  passing by reference ⬇️ so copies are not made at each function call
-void helper(string &digits, int i, string &combi){
-	if(i == size(digits)) {
-		ans.push_back(combi);    
-		return;
-	}
-	for(auto c : mappings[digits[i] - '2']){
-		combi.push_back(c);            // add a character from mappings for current position,
-		helper(digits, i + 1, combi);  // and recurse for next positions
-		combi.pop_back();              // backtrack
-	}   
-}
+    vector<string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> ans;
+    
+    vector<string> letterCombinations(string digits) 
+    {
+        if(digits == "") return ans;        
+        string combination = "";
+        helper(digits, 0, combination);
+        return ans; 
+    }
+// passing by reference ⬇️ so copies are not made at each function call
+    void helper(string &digits, int i, string &combi)
+    {
+        if(i == size(digits)) 
+        {
+            ans.push_back(combi);    
+            return;
+        }
+        for(auto c : mappings[digits[i] - '2']){
+            combi.push_back(c);            // add a character from mappings for current position,
+            helper(digits, i + 1, combi);  // and recurse for next positions
+            combi.pop_back();              // backtrack
+        }   
+    }
 };
 
 
