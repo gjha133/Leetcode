edef9d2c599e8364b9d650563640a44fb4f2ba20
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-13 23:23:55 +0530

    Time: 3 ms (54.99%), Space: 7.9 MB (25.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,16 +4,19 @@ public:
         int n = nums.size();
         if(!n) return 0;
         vector<int> dp(n);
-        dp[0] = nums[0];
+        int prev = nums[0], prev2 = 0;
         
         for(int i = 1; i<n; i++)
         {
             int take = nums[i];
-            if(i > 1) take += dp[i-2];
-            int nottake = dp[i-1];
-            dp[i] = max(take, nottake);
+            if(i > 1) take += prev2;
+            int nottake = prev;
+            int cur = max(take, nottake);
+            
+            prev2 = prev;
+            prev = cur;
         }
-        return dp[n-1];
+        return prev;
     }
 
 };
