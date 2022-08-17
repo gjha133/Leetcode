611e6337284853f18035fed04258e1c0bb3f63ef
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-17 16:02:01 +0530

    Time: 30 ms (49.91%), Space: 9 MB (38.10%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.size() == 0) return 0;
-        unordered_map<char, int> mp;
-        int mx = INT_MIN, i = 0, j = 0, n = s.size();
+        int n = s.size();
+        if(!n) return 0;
+        unordered_map<char, int> m;   //create map to store frequency,(get to know all unique characters
+        int i = 0, j = 0, ans = INT_MIN; 
         
-        while( j < n )
+        while(j < n)   
         {
-            mp[s[j]]++;
-            while(mp.size() < j - i + 1)
+            m[s[j]]++;                      // increase the frequency of the element as you traverse the string
+            if(m.size() == j - i + 1)       // when map size is equal to the window size means suppose window size is 3 and map size is also three that means                                                  in map all unique characters are their
             {
-                mp[s[i]]--;
-                if(mp[s[i]] == 0) mp.erase(s[i]);
-                i++;
+                ans = max(ans, j - i + 1); 
             }
-            if(mp.size() == j - i + 1)
+            // If the map size is less than the window size means there is some duplicate present like window size = 3 and map                                                 size = 2 means there is a duplicates
+            while(m.size() < j - i + 1)           //so till the duplicates are removed completely
             {
-                mx = max(mx, j - i + 1);
+                m[s[i]]--;              
+                if(m[s[i]] == 0) m.erase(s[i]);        
+                i++;                     
             }
-            j++;
+            j++;                       
         }
-        
-        return mx;
+        return ans;
     }
 };
