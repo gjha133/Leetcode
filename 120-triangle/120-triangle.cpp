ffaa3fb1bd13fb6ead3e4caa8d816fead80d4bbc
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-20 07:01:40 +0530

    Time: 10 ms (54.76%), Space: 8.8 MB (59.51%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp (n, vector<int> (n, -1));
-        return minTotal(0, 0, triangle, dp);
-    }
-    
-    int minTotal(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp)
-    {
-        if(i == triangle.size() - 1) return triangle[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
+        int dp[n][n];
         
-        int down = minTotal(i + 1, j, triangle, dp);
-        int diagonal = minTotal(i + 1, j + 1, triangle, dp);
+        // Base Case
+        for(int j = 0; j < n; j++)
+        {
+            dp[n-1][j] = triangle[n-1][j];
+        }
         
-        return dp[i][j] = triangle[i][j] + min(down, diagonal);
+        for(int i = n-2; i >= 0; i--)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                int down = dp[i+1][j];
+                int diagonal = dp[i+1][j+1];
+                dp[i][j] = triangle[i][j] + min(down, diagonal);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
+    
