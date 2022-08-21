de28bd6c1fdaf68963c9887450b2577e8fc2781b
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-21 23:50:04 +0530

    Time: 558 ms (20.11%), Space: 12.5 MB (56.94%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -4,26 +4,32 @@ public:
         int totalSum = 0;
         for(int num : nums) totalSum += num;
         if(totalSum&1) return false;
+        
         return subsetSumToK(nums.size(), totalSum/2, nums) == true;
-    }
-    
-    bool sumK(int i, int k, vector<int> &arr, vector<vector<int>> &dp)
-    {
-        if(k == 0) return true;
-        if(i == 0) return arr[i] == k;
-        if(dp[i][k] != -1) return dp[i][k];
-
-        bool notTake = sumK(i-1, k, arr, dp);
-        bool take = false;
-        if(k >= arr[i]) take = sumK(i-1, k - arr[i], arr, dp);
-
-        return dp[i][k] = notTake or take;
     }
 
     bool subsetSumToK(int n, int k, vector<int> &arr) 
     {
-        vector<vector<int>> dp (n, vector<int> (k+1, -1));
-        return sumK(n-1, k, arr, dp);
+        vector<vector<bool>> dp (n, vector<bool> (k+1, false));
+        
+        // With sum = 0, entry = 1.
+        for(int i=0; i<n; i++) dp[i][0] = true;
+        
+        if(arr[0] <= k) dp[0][arr[0]] = true;
+        
+        for(int i = 1; i<n; i++)
+        {
+            for(int j = 1; j <=k; j++)
+            {
+                bool notTake = dp[i-1][j];
+                bool take = false;
+                if(arr[i] <= j) take = dp[i-1][j - arr[i]];
+                
+                dp[i][j] = take or notTake;
+            }
+        }
+        
+        return dp[n-1][k];
     }
 
 
