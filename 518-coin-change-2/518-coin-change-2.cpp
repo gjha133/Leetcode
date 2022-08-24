001b6f022a56caed99a54e5e3a6f2a054de453c8
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-24 09:54:26 +0530

    Time: 92 ms (16.97%), Space: 18.5 MB (44.67%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp (n, vector<int> (amount + 1, -1));
-        return f(n-1, amount, coins, dp);
+        vector<vector<int>> dp (n, vector<int> (amount + 1, 0));
+        
+        for(int i = 0; i < n; i++) dp[i][0] = 1;
+        
+        for(int j = 1; j <= amount; j++) if(j % coins[0] == 0) dp[0][j] = 1;
+        
+        for(int i = 1; i < n; i++)
+        {
+            for(int target = 1; target <= amount; target++)
+            {
+                int notTake = dp[i-1][target];
+                int take = 0;
+                if(target >= coins[i]) take = dp[i][target - coins[i]];
+                
+                dp[i][target] = notTake + take;
+            }
+        }
+        
+        return dp[n-1][amount];
     }
     
-    int f(int i, int target, vector<int>& coins, vector<vector<int>>& dp)
-    {
-        if(target == 0) return 1;
-        if(i == 0) return target % coins[i] == 0; 
-        
-        if(dp[i][target] != -1) return dp[i][target];
-        
-        int notTake = f(i - 1, target, coins, dp);
-        int take = 0;
-        if(target >= coins[i]) take = f(i, target - coins[i], coins, dp);
-        
-        return dp[i][target] = take + notTake;
-    }
 };
