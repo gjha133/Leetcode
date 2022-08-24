13c6b09f9b87c5cb2e4e811ad3435751766df40b
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-24 20:52:12 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (36.28%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<int> dp (n + 1, -1);
-        return f(0, days, costs, dp);
-    }
-    
-    int f(int i, vector<int>& days, vector<int>& costs, vector<int>& dp)
-    {
-        int n = days.size();
-        if(i >= n) return 0;
+        vector<int> dp (n + 1, 0);
         
-        if(dp[i] != -1) return dp[i];
-                
-        int day_pass = costs[0] + f(i + 1, days, costs, dp);
+        dp[n] = 0;
         
-        int d;
-        for(d = i ; d < n and days[d] < days[i] + 7; d++);
-        int week_pass = costs[1] + f(d, days, costs, dp);
+        for(int i = n-1; i >= 0; i--)
+        {
+            int day_pass = costs[0] + dp[i+1];
+            
+            int d;
+            for(d = i ; d < n and days[d] < days[i] + 7; d++);
+            int week_pass = costs[1] + dp[d];
+
+            for(d = i ; d < n and days[d] < days[i] + 30; d++);
+            int month_pass = costs[2] + dp[d];
+            
+            dp[i] = min(day_pass, min(week_pass, month_pass));
+        }
         
-        for(d = i ; d < n and days[d] < days[i] + 30; d++);
-        int month_pass = costs[2] + f(d, days, costs, dp);
-        
-        return dp[i] = min(day_pass, min(week_pass, month_pass));
+        return dp[0];
     }
 };
