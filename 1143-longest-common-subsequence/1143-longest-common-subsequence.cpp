3a9fca9cec3588819a733fc93025352b85e1520d
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-26 05:45:41 +0530

    Time: 75 ms (8.05%), Space: 13.3 MB (11.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,25 +3,25 @@ public:
     int longestCommonSubsequence(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp (n, vector<int> (m, -1));
-        return fmemo(n-1, s, m-1, t, dp);
+        vector<vector<int>> dp (n + 1, vector<int> (m + 1, -1));
+        return fmemo(n, s, m, t, dp);
     }
     
     int f(int i, string &s, int j, string& t)
     {
-        if(i < 0 or j < 0) return 0;
+        if(i == 0 or j == 0) return 0;
 
-        if(s[i] == t[j]) return 1 + f(i-1, s, j-1, t);
+        if(s[i-1] == t[j-1]) return 1 + f(i-1, s, j-1, t);
 
         return max(f(i-1, s, j, t), f(i, s, j-1, t));
     }
 
     int fmemo(int i, string &s, int j, string& t, vector<vector<int>>& dp)
     {
-        if(i < 0 or j < 0) return 0;
+        if(i == 0 or j == 0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(s[i] == t[j]) return dp[i][j] = 1 + fmemo(i-1, s, j-1, t, dp);
+        if(s[i-1] == t[j-1]) return dp[i][j] = 1 + fmemo(i-1, s, j-1, t, dp);
 
         return dp[i][j] = max(fmemo(i-1, s, j, t, dp), fmemo(i, s, j-1, t, dp));
     }
