2a06c32f339831050a68b45aa5b91c7089a455b1
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-28 02:52:07 +0530

    Time: 109 ms (41.85%), Space: 9.5 MB (78.16%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         int n = p.size(), m = s.size();
-        vector<vector<int>> dp (n + 1, vector<int> (m + 1, -1));
+        vector<vector<bool>> dp (n + 1, vector<bool> (m + 1, 0));   // bool for ftab
         // return f(p, s, n-1, m-1);
-        return fmemo(p, s, n, m, dp);
+        // return fmemo(p, s, n, m, dp);
+        return ftab(p, s, n, m, dp);
     }
     
     bool f(string &pattern, string &text, int i, int j)
@@ -48,5 +49,42 @@ public:
             return dp[i][j] = fmemo(pattern,text,i-1,j, dp) or fmemo(pattern,text,i,j-1, dp);
         }
         return dp[i][j] = false;
+    }
+    
+    bool ftab(string &pattern, string &text, int i, int j, vector<vector<bool>>& dp)
+    {
+        int n = pattern.size(), m = text.size();
+        
+        // Base Case
+        dp[0][0] = true;
+        for(int j = 1; j <= m; j++) dp[0][j] = false;
+        for(int i = 1; i <= n; i++)
+        {
+            bool flag = true;
+            for(int q = 1; q <= i; q++)
+            {
+                if(pattern[q-1] != '*')
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;
+        }
+        
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                if(pattern[i-1] == text[j-1] or pattern[i-1] == '?') dp[i][j] = dp[i-1][j-1];
+                else if(pattern[i-1] == '*')
+                {
+                    dp[i][j] = dp[i-1][j] or dp[i][j-1];
+                }
+                else dp[i][j] = false;
+            }
+        }
+        
+        return dp[n][m];
     }
 };
