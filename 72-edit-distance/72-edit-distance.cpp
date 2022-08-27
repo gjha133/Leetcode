fb3f6d1b1b1ce516fec0640f41139ab3f2be5cf0
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-27 15:54:35 +0530

    Time: 18 ms (63.01%), Space: 9.2 MB (19.75%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -23,5 +23,30 @@ public:
         }
     }
     
+    int ftab(int i, int j, string& word1, string& word2, vector<vector<int>>& dp)
+    {
+        int n = word1.size(), m = word2.size();
+        
+        for(int i = 0; i <= n; i++) dp[i][0] = i;
+        for(int j = 0; j <= m; j++) dp[0][j] = j;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else
+                {
+                    int insert  = 1 + dp[i][j-1];
+                    int del     = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+                    dp[i][j] = min(insert, min(del, replace));
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];    
+    }
+    
     
 };
