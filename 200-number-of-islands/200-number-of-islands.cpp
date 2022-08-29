5238304432d4e261857f524a764e14e77aaed15e
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-29 08:35:59 +0530

    Time: 40 ms (86.31%), Space: 12.4 MB (55.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int rows = grid.size();
-        if(!rows) return 0;
-        int cols =  grid[0].size();
-        int count = 0;
+        int rows = grid.size(), cols =  grid[0].size();
         
-        for(int i=0; i<rows; i++)
+        int count = 0;       
+        for(int i = 0; i < rows; i++)
         {
-            for(int j=0; j<cols; j++)
+            for(int j = 0; j < cols; j++)
             {
                 if(grid[i][j] == '1')
                 {
+                    // DFS
                     markCurrentIsland(i, j, rows, cols, grid);
                     count++;
                 }
@@ -25,7 +24,7 @@ private:
     void markCurrentIsland(int x, int y, int row, int col, vector<vector<char>>& grid)
     {
         // Boundary Case
-        if( x<0 or x>=row or y<0 or y>=col or grid[x][y] != '1') return;
+        if( x < 0 or x >= row or y < 0 or y >= col or grid[x][y] != '1') return;
         
         grid[x][y] = '2';
         markCurrentIsland(x+1, y, row, col, grid);      // Down
