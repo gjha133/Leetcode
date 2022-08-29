d04dc90dc78c76cddf55575a81bd4e4ede865030
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-08-29 12:33:22 +0530

    Time: 607 ms (64.57%), Space: 159.5 MB (62.03%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,101 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int amountOfTime(TreeNode* root, int start) {
+        int time = 0;    
+        
+        unordered_map<TreeNode*,TreeNode*> parent;
+        markParent(root,parent);
+        
+        TreeNode* target = findNode(root,start);
+        
+        queue<TreeNode*> q;
+        unordered_map<TreeNode*, bool> vis;       
+
+        q.push(target);
+        vis[target] = true;
+
+        while(!q.empty())
+        {
+            int n = q.size();
+            while(n--)
+            {
+                auto node = q.front();
+                q.pop();
+                vis[node] = true;
+
+                if(node->left and !vis[node->left])
+                {
+                    q.push(node->left);
+                    vis[node->left] = true;
+                }
+
+                if(node->right and !vis[node->right])
+                {
+                    q.push(node->right);
+                    vis[node->right] = true;
+                }
+
+                if(parent[node] and !vis[parent[node]])
+                {
+                    q.push(parent[node]);
+                    vis[parent[node]] = true;
+                }
+            }
+            
+            time++;
+        }
+        
+        return time-1;
+    }
+
+    
+    
+private:
+    TreeNode* findNode(TreeNode* node, int key) 
+    {
+        if(node)
+        {
+            if(node->val == key) return node;        
+            else {
+                TreeNode* foundNode = findNode(node->left,key);
+                if(!foundNode) foundNode = findNode(node->right,key);              
+                return foundNode;
+             }
+        } 
+        else return NULL;
+
+    }
+
+    void markParent(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &parent)
+    {
+        if(!root)return;
+
+        if(root->left) parent[root->left]=root;     
+        if(root->right) parent[root->right]=root;
+
+        markParent(root->left,parent);
+        markParent(root->right,parent);
+    }
+    
+};
+
+
+
+
+
+
+
+
+
+
