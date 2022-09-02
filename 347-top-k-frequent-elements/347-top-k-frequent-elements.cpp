f07b1e7a0df05c543d50cb92ef60fc81853e4e00
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-09-02 09:36:18 +0530

    Time: 21 ms (70.98%), Space: 13.7 MB (44.53%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> cnt;
-        for (int x : nums) cnt[x] += 1;
-
-        vector<pair<int, int>> maxHeap;
-        for (auto [num, freq] : cnt) maxHeap.emplace_back(freq, num);
-        make_heap(maxHeap.begin(), maxHeap.end()); // default is maxHeap
-
-        vector<int> ans;
-        while (k-- > 0) {
-            auto [_, num] = maxHeap[0];
-            pop_heap(maxHeap.begin(), maxHeap.end());
-            maxHeap.pop_back(); // delete the unused last element
-
-            ans.push_back(num);
+        for (int num : nums) cnt[num]++;
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+        
+        for(auto it : cnt)
+        {
+            minHeap.push({it.second, it.first});
+            if(minHeap.size() > k) minHeap.pop();
         }
+        
+        vector<int> ans;
+        while(minHeap.size())
+        {
+            ans.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+
         return ans;
+        
     }
 };
