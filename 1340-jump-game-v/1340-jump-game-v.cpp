2154ba6a4541dfa447562d7c2de4612925ca20be
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-09-06 19:42:32 +0530

    Time: 51 ms (74.71%), Space: 15 MB (33.01%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -8,25 +8,25 @@ public:
         {
             ans = max(ans, f(i, d, arr, dp));
         }
-        
+
         return ans;
     }
-    
+
     int f(int i, int d, vector<int>& arr, vector<int>& dp)
     {
         int n = arr.size();
         int ans = 1;
-        
+
         if(dp[i] != -1) return dp[i];
-        
+
         // Jump Right
         for(int j = i + 1; j <= min(i+d, n-1) and arr[i] > arr[j]; j++)
             ans = max(ans, 1 + f(j, d, arr, dp));
-        
+
         // Jump left
         for(int j = i - 1; j >= max(0, i-d) and arr[i] > arr[j]; j--)
             ans = max(ans, 1 + f(j, d, arr, dp));
-        
+
         return dp[i] = ans;
     }
 };
