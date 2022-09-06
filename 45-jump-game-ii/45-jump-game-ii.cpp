11af6b0695866808bc36463a2ff3457ab842362e
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-09-06 09:57:41 +0530

    Time: 432 ms (34.99%), Space: 17.2 MB (28.32%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp (n, -1);
-        return f(0, nums, dp);
-    }
-    
-    int f(int i, vector<int>& nums, vector<int>& dp)
-    {
-        int n = nums.size();
-        if(i >= n-1) return 0;
+        vector<int> dp (n, 0);
         
-        if(dp[i] != -1) return dp[i];
+        for(int i = n-2; i >= 0; i--)
+        {
+            if(nums[i] >= n - 1 - i) dp[i] = 1;
+            else 
+            {
+                int mn = 1e9;
+                for(int j = i + 1; j <= i + nums[i]; j++)
+                    mn = min(mn, 1 + dp[j]);
+
+                dp[i] = mn;
+            }            
+        }
         
-        int mn = 1e9;
-        for(int j = i + 1; j <= i + nums[i]; j++)
-            mn = min(mn, 1 + f(j, nums, dp));
-        
-        return dp[i] = mn;
+        return dp[0];
     }
 };
