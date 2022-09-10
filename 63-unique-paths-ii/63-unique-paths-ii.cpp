9cbe99d5b334e099560f8466c70a89a04db5522f
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-09-10 11:33:36 +0530

    Time: 7 ms (40.79%), Space: 7.6 MB (91.80%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp (m, vector<int> (n, -1));
-        return countUnique(m-1, n-1, grid, dp);
+        int dp[m][n];
+    
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 and j == 0) dp[0][0] = 1;
+                else
+                {
+                    int up = 0, left = 0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }                
+            }
+        }
+        return dp[m-1][n-1];
     }
     
-    int countUnique(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp)
-    {
-        if(i == 0 and j == 0 and !grid[i][j]) return 1;
-        if(i < 0 or j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(grid[i][j]) return 0;
-        int up = countUnique(i-1, j, grid, dp);
-        int left = countUnique(i, j-1, grid, dp);
-        
-        return dp[i][j] = up + left;
-    }
+    
 };
