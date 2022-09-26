fc2d2a3fff73bea8e71278a56a86768dcc16d13c
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-09-26 15:22:55 +0530

    Time: 247 ms (29.10%), Space: 53.4 MB (56.12%) - LeetHub

---
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        sort(piles.begin(), piles.end());
+        int ans = 0, n = piles.size();
+        for(int i = n-2; i >= n/3; i -= 2)
+            ans += piles[i];
+        
+        return ans;
+    }
+};
