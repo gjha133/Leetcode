77041c378d4df5ee6ecfe56151351fa4c675c2a3
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-10-09 06:54:28 +0530

    Time: 4 ms (21.78%), Space: 5.9 MB (73.92%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int ans = 1;
+        for(int i = 2; i <= gcd(a, b); i++)
+            if(a % i == 0 and b % i == 0) ans++;
+        return ans;        
+    }
+    
+private:
+    int gcd(int a, int b)
+    {
+        int result = min(a, b);
+        while(result > 0)
+        {
+            if(a % result == 0 and b % result == 0) break;
+            result--;
+        }
+        return result;
+    }
+    
+    
+};
