9eac9d3aaeb77f9e482b60b92fd97d52920bf4a9
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-10-25 16:10:53 +0530

    Time: 68 ms (69.19%), Space: 21.3 MB (96.75%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        int initialSum = accumulate(nums.begin(), nums.end(), 0);
+        long long Sn = n*(n+1)/2;
+        long long SSn = Sn*(2*n+1)/3;
         
-        set<int> set;
-        for(int num : nums) set.insert(num);
-        int sum = accumulate(set.begin(), set.end(), 0);
+        long long X = 0, Y = 0;     // X = missing, Y = repeating
         
-        int Sn = n*(n+1)/2;
+        for(int num : nums)
+        {
+            Sn -= num;
+            SSn -= num*num;
+        }
         
+        X = (Sn + SSn/Sn)/2;
+        Y = X - Sn;
         
-        return {initialSum - sum, Sn - sum};
-        
+        return {(int)Y, (int)X};
         
     }
 };
