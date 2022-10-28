dc9f7ceeb45cdbac0a433242593a7297c45ecf45
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-10-28 23:26:44 +0530

    Time: 26 ms (63.45%), Space: 13.2 MB (55.26%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,64 +2,35 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& arr) {
         int n = arr.size(), m = arr[0].size();
-        bool rowZero = false, colZero = false;
-        
-        // Check in first row
-        for(int j = 0; j < m; j++)
-        {
-            if(arr[0][j] == 0)
-            {
-                rowZero = true;
-                break;
-            }
-        }
-        
-        // Check in first col
+        vector<int> rows(n, 1), cols(m, 1);
+
         for(int i = 0; i < n; i++)
         {
-            if(arr[i][0] == 0)
-            {
-                colZero = true;
-                break;
-            }
-        }
-        
-        // Except first row and col
-        for(int i = 1; i < n; i++)
-        {
-            for(int j = 1; j < m; j++)
+            for(int j = 0; j < m; j++)
             {
                 if(arr[i][j] == 0)
                 {
-                    arr[i][0] = 0;
-                    arr[0][j] = 0;
+                    rows[i] = 0;
+                    cols[j] = 0;
                 }
-                    
             }
         }
-        
-        // Mark as 0 for apt case
-        for(int i = 1; i < n; i++)
+
+        for(int i = 0; i < n; i++)
         {
-            for(int j = 1; j < m; j++)
+            if(rows[i] == 0)
             {
-                if(arr[0][j] == 0 or arr[i][0] == 0)
+                for(int j = 0; j < m; j++)
                     arr[i][j] = 0;
             }
         }
         
-        if(rowZero) //handle the first row
-        { 
-            for (int i = 0; i < m; i++) 
+        for(int j = 0; j < m; j++)
+        {
+            if(cols[j] == 0)
             {
-                arr[0][i] = 0;
-            }
-        }
-        if (colZero) //handle the first column
-        { 
-            for (int i = 0; i < n; i++)
-            {
-                arr[i][0] = 0;
+                for(int i = 0; i < n; i++)
+                    arr[i][j] = 0;
             }
         }
     }
