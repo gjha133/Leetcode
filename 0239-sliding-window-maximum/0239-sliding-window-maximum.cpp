ee2a6f351562b3c089eebf754e3bf1edf237f3f3
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-11-01 01:14:24 +0530

    Time: 395 ms (81.73%), Space: 134.7 MB (80.89%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> dq;  //We are storing index in deque
+        
+        int n = nums.size();
+        
+        for(int j=0; j<n; j++)
+        {
+            // For out of bound element (from the left of array, as we will slide the window)
+            if(!dq.empty() and dq.front() == j-k) // If the index at front is out of bound
+                dq.pop_front();
+            
+             // Removing elements which are smallers than the current element
+            while(!dq.empty() and nums[dq.back()] <= nums[j])
+                dq.pop_back();
+            
+            dq.push_back(j);
+            if(j >= k-1) ans.push_back(nums[dq.front()]);
+        }
+        
+        return ans;
+    }
+};
