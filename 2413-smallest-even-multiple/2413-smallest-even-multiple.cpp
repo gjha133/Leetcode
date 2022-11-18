7c6440a72a8e508f6bf797fb3b7ff8ed563f0c47
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-11-18 22:13:38 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.25%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -3,4 +3,14 @@ public:
     int smallestEvenMultiple(int n) {
         return lcm(2, n);
     }
+    
+    int gcd(int a, int b)
+    {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    
+    int lcm(int a, int b)
+    {
+        return (a*b)/gcd(a, b);
+    }
 };
