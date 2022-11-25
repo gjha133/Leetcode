a532270bf3e4a4d5c27fda6a5fd843bd009c16de
gjha133 <53974342+gjha133@users.noreply.github.com>
2022-11-25 11:33:19 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (90.66%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long ncr(int n, int r) {
+        long ans = 1;
+        for(int i = 0; i < r; i++) {
+            ans *= n-i;
+            ans /= i+1;
+        }
+        return ans;   
+    }
+    int numTrees(int n) {
+        return ncr(2*n, n) / (n + 1);               
+        
+        //      2n
+        //       C   / (n+1)
+        //        n
+    }
+};
