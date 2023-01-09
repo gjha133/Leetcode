1ffeb24cafa94705ca51b278b8216c0052d792b5
Gautam Jha <53974342+gjha133@users.noreply.github.com>
2023-01-09 21:21:33 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.cpp
+++ Bubble Sort - GFG/bubble-sort.cpp
@@ -3,6 +3,8 @@
 
 // C program for implementation of Bubble sort
 #include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 // swapping the elements
 void swap(int *xp, int *yp)
@@ -23,14 +25,12 @@ class Solution
     void bubbleSort(int arr[], int n)
     {
         // Your code here  
-        for(int i = 0; i < n-1; i++)
-        {
-            for(int j = 0; j < n-i-1; j++)
-            {
-                if(arr[j] > arr[j+1])
-                    swap(&arr[j], &arr[j+1]);
-            }
-        }
+        for (int i = 0; i < n - 1; i++)
+        // Last i elements are already in place
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1])
+                    swap(arr[j], arr[j + 1]);
+        
     }
 };
 
