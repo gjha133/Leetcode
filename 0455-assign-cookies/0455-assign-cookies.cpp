46b7cd742659e1a1b575bdcd5c6c6da9d83a65db
Gautam Jha <53974342+gjha133@users.noreply.github.com>
2023-04-20 04:26:20 +0530

    Time: 49 ms (8.40%), Space: 17.5 MB (86.19%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
+    
+    bool static comp(int &a, int &b)
+    {
+        return a > b;
+    }
+    
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        sort(s.rbegin(), s.rend());
-        sort(g.rbegin(), g.rend());
+        // sort(s.rbegin(), s.rend());
+        // sort(g.rbegin(), g.rend());
+        
+        sort(s.begin(), s.end(), comp);
+        sort(g.begin(), g.end(), comp);
         
         int sL = s.size(), gL = g.size();
         int idx = 0, happy = 0;
