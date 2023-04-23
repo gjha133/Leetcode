ce3d6776fd6fcede453a87b72c1eeffbf895057b
Gautam Jha <53974342+gjha133@users.noreply.github.com>
2023-04-24 02:31:34 +0530

    Time: 13 ms (62.82%), Space: 6 MB (92.83%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    int romanToInt(string str) {
+        int ans = 0;        // To store final ans
+        int prev = 0;       // To store value of last char
+        
+        for(int i=0; i<str.size(); i++)
+        {
+            char curr = str[i];
+            int val = 0;
+            
+            switch(curr)
+            {
+                case 'I': val = 1; break;
+                case 'V': val = 5; break;
+                case 'X': val = 10; break;
+                case 'L': val = 50; break;
+                case 'C': val = 100; break;
+                case 'D': val = 500; break;
+                case 'M': val = 1000; break;
+                default: return 0;
+            }
+            
+            ans += val;                 // Add the value of current char to the final ans
+            if(prev < val)              // If last char value is smaller than curr then reduce the value of final ans with twice the
+                ans -= prev*2;          // value of the last char to remove the effect of last char
+                                        // XIX -> 10 + 1 = 11 (X+I). 11 - 1*2 = 9 (As I before X). 9 + 10 = 19. Final ans
+            prev = val;     
+        }
+        
+        return ans;
+    }
+};
+
+
+
+
+
+
+/*class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i] == 'I')
+            {
+                if(s[i+1] == 'V')               {ans += 4;    i++;}
+                else if(s[i+1] == 'X')          {ans += 9;    i++;}
+                else ans += 1;
+            }
+            else if(s[i] == 'V') ans += 5;
+            else 
+                if(s[i] == 'X')
+                {
+                    if(s[i+1] == 'L')           {ans += 40;   i++;}
+                    else if(s[i+1] == 'C')      {ans += 90;   i++;}
+                    else ans += 10;
+                }
+            else if(s[i] == 'L') ans += 50;
+            else 
+                if(s[i] == 'C')
+                {
+                    if(s[i+1] == 'D')           {ans += 400;  i++;}
+                    else if(s[i+1] == 'M')      {ans += 900;  i++;}
+                    else ans += 100;
+                }
+            else if(s[i] == 'D') ans += 500;
+            else if(s[i] == 'M') ans += 1000;
+        }
+        
+        return ans;
+    }
+};
+*/
+
