8454098b90f7e1ef0e2d60c1d413bf46852df646
Gautam Jha <53974342+gjha133@users.noreply.github.com>
2023-05-06 04:32:16 +0530

    Time: 15 ms (95.76%), Space: 10 MB (28.50%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -4,6 +4,8 @@ public:
         int mx = 0, i = 0, j = 0, n = s.size(), count = 0;
         while(j < n) {
             if(isVowel(s[j])) count++;
+            // If windows size gets bigger than given length, decrease window length 
+            // from beginning and reduce counnt if ith char is a vowel
             if(j - i + 1 > k)
             {
                 if(isVowel(s[i])) count--;
